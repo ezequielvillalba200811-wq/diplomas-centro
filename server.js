81b0456ab6e4acab4d814a915c02450c955c364f
ezequielvillalba200811-wq <ezequielvillalba2008.11@gmail.com>
2026-09-07 13:59:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.static(__dirname));
 const PORT = process.env.PORT || 3000;
 
 const DATA_FILE = path.join(__dirname, 'certificados.json');
